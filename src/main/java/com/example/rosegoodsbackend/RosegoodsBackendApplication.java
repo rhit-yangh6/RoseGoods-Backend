e49d3e11762b2ddb38f86876d2ddef5444de45f4
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-09-17 09:16:27 -0400

    init

---
+++ src/main/java/com/example/rosegoodsbackend/RosegoodsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rosegoodsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosegoodsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RosegoodsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rosegoodsbackend/RosegoodsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rosegoodsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosegoodsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
